5775a34490bb917ed5d43b8517f6f51126c651ab
Daniela Cosmina Weigel <daniela.c.weigel@gmail.com>
2018-07-26 16:54:51 +0300

    Updated service-worker

--- js/SW.js
+++ js/SW.js
@@ -1,26 +1,28 @@
+var cacheName = static_version;
+
 self.addEventListener('install', function(event){
   event.waitUntil(
-    caches.open('app-static-v1').then(function(cache){
+    caches.open('static_version').then(function(cache){
       return cache.addAll([
-        '/',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg',
-        'img/food.jpg',
-        'css/styles.css',
-        'css/responsive.css'
-        'js/dbhelper.js',
-        'js/main.js',
-        'js/restaurant_info.js',
-        'index.html',
-        'restaurant.html'
+        "/Thenootza's Restaurant Stage -1 Master/",
+        "/Thenootza's Restaurant Stage -1 Master/css/styles.css",
+        "/Thenootza's Restaurant Stage -1 Master/css/responsive.css",
+        "/Thenootza's Restaurant Stage -1 Master/js/dbhelper.js",
+        "/Thenootza's Restaurant Stage -1 Master/js/main.js",
+        "/Thenootza's Restaurant Stage -1 Master/js/restaurant_info.js",
+        "/Thenootza's Restaurant Stage -1 Master/index.html",
+        "/Thenootza's Restaurant Stage -1 Master/restaurant.html",
+        "/Thenootza's Restaurant Stage -1 Master/img/1.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/2.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/3.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/4.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/5.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/6.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/7.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/8.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/9.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/10.jpg",
+        "/Thenootza's Restaurant Stage -1 Master/img/food.jpg"
       ]);
     })
   );
